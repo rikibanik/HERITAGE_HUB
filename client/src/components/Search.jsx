62f98c78c8932e0b791377b4387e9d5eec19e549
Sanu Kumar <145834771+sk66641@users.noreply.github.com>
2025-01-28 19:30:59 +0530

    Update Search.jsx

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -37,7 +37,7 @@ const Search = () => {
     const handleOnChange = async (searchData) => {
         if (searchData && searchData.details) {
             const { _id, name } = searchData.details;
-            window.location.href = `/museum?name=${encodeURIComponent(name)}&id=${encodeURIComponent(_id)}`;
+            navigate(`/museum?name=${encodeURIComponent(name)}&id=${encodeURIComponent(_id)}`);
         }
     };
 

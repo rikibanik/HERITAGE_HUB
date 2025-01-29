081d09a7b0b8296fab1cf29a5d0407cefbe9c73a
RIKI BANIK <76590963+rikibanik@users.noreply.github.com>
2025-01-29 16:38:28 +0530

    cors check

--- server/app.js
+++ server/app.js
@@ -10,16 +10,14 @@ const adminRoutes = require('./routes/adminRoutes');
 const userRoutes = require('./routes/userRoutes');
 const authorRoutes = require('./routes/authorRoutes')
 const paymentRoutes = require('./routes/paymentRoutes');
-app.use(cors({
-  origin: (origin, callback) => {
-    // if (origin === 'http://localhost:5174' || origin === 'http://another-origin.com') {
-      callback(null, true); // Allow the request
-    // } else {
-    //   callback(new Error('Not allowed by CORS')); // Block the request
-    // }
-  },
-  credentials: true, // Allow cookies and credentials
-}));
+
+app.use(
+  cors({
+    origin: "https://heritage-hub-frontend.onrender.com", // Allow only your frontend
+    credentials: true, // Allow cookies and authentication headers
+  })
+);
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

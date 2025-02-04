73cb5e7439bebc5e0735554c12fe7367027cb728
RIKI BANIK <76590963+rikibanik@users.noreply.github.com>
2025-02-04 15:49:45 +0530

    mongoose update

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -5,5 +5,6 @@ const orderController = require('../controller/orderController')
 router.post('/booknow',authMiddleware.authUser ,orderController.createOrder);
 router.post('/verify-payment', authMiddleware.authUser, orderController.verifyPayment)
 router.post('/order-details/:id',authMiddleware.authUser,orderController.getOrderbyId)
-router.get('/myorders',authMiddleware.authUser,orderController.getMyOrders)
+router.get('/myorders',authMiddleware.authUser,orderController.getMyOrders);
+router.post('/cancel',authMiddleware.authUser,orderController)
 module.exports = router;

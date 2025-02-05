52af29ac3f108d9356de06ee52e4bbfb0df85823
Sanu Kumar <145834771+sk66641@users.noreply.github.com>
2025-02-05 17:20:19 +0530

    Update Booking.jsx

--- client/src/components/museum/Booking.jsx
+++ client/src/components/museum/Booking.jsx
@@ -317,14 +317,25 @@ const Booking = () => {
                         {/* Booking Button */}
                         <button
                             className="w-full bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-3 px-8 rounded-lg transition duration-300 flex justify-center items-center"
+                            disabled={loading} // Ensure button is disabled when loading
                         >
-                            {calculatePrice() === 0 ? "Buy now" : "Pay now"}
+                            {loading ? (
+                                <>
+                                    <svg className="animate-spin h-5 w-5 mr-2 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                        <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"
+                                        ></circle>
+                                        <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8v8H4z"
+                                        ></path>
+                                    </svg>
+                                    Paying...
+                                </>
+                            ) : calculatePrice() === 0 ? "Buy now" : "Pay now"}
                         </button>
+
 
                     </form>
                 </div >
             </section >
-            {loading && <PaymentShimmer/>}
             {confirmOrder && <SuccessBookingPopup selectedSlot={selectedSlot} availableSlots={availableSlots} />}
 
         </>

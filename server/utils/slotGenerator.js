6c022ea6f67839649493cac7feaba7bfdad23fcf
RIKI BANIK <76590963+rikibanik@users.noreply.github.com>
2025-03-23 12:26:45 +0530

    UPDATE SLOT GENRATING ALGO

--- server/utils/slotGenerator.js
+++ server/utils/slotGenerator.js
@@ -1,77 +1,76 @@
-const slotting = require('../services/slotting');
+const slotting = require("../services/slotting");
 const cron = require("node-cron");
-const venueModel = require('../db/models/venueModel');
+const venueModel = require("../db/models/venueModel");
 
 const autoGenerateSlots = async () => {
-    try {
-        console.log('Generating slots...');
-        const maxCapacity = 200;
-        const elasticCapacity = 50;
-        const dateTime = new Date();
-        const date = dateTime.toISOString().split('T')[0]; // YYYY-MM-DD format
-        const museums = await venueModel.find({}, '_id workingHours');
+  try {
+    console.log("Generating slots...");
+    const maxCapacity = 200;
+    const elasticCapacity = 50;
+    const dateTime = new Date().toLocaleString("en-US", {
+      timeZone: "Asia/Kolkata",
+    });
 
-        for (const museum of museums) {
-            
-            const { opening, closing } = museum.workingHours;
-            const openTime = opening.hour ;
-            const closeTime = (closing.hour>=12)? closing.hour: closing.hour + 12;
-            
-            const slotDuration = museum.slotDuration || 120; // Default to 60 minutes
-          
+    // Convert the string back to a Date object
+    const istDate = new Date(dateTime);
 
-            var startTime = openTime;
-            var endTime = openTime + slotDuration/60;
+    // Format to YYYY-MM-DD
+    const date = istDate.toISOString().split("T")[0];
+    const museums = await venueModel.find({}, "_id workingHours");
 
-        
-            
-            while (endTime <= closeTime) {
-                const slots = {
-                    startTime: { hour: startTime, minute: 0 },
-                    endTime: { hour: endTime, minute: 0 }
-                };
-                const slot = {
-                    venueId: museum._id,
-                    date,
-                    slots,
-                    maxCapacity,
-                    elasticCapacity
-                };
-                await slotting.createSlot(slot);
-                startTime = endTime;
-                endTime = startTime + slotDuration/60;
-            }
-        }
+    for (const museum of museums) {
+      const { opening, closing } = museum.workingHours;
+      const openTime = opening.hour;
+      const closeTime = closing.hour >= 12 ? closing.hour : closing.hour + 12;
 
-        console.log('Slots generated successfully');
-        return true;
-    } catch (error) {
-        console.error('Error generating slots:', error);
-        return 0;
+      const slotDuration = museum.slotDuration || 120; // Default to 60 minutes
+
+      var startTime = openTime;
+      var endTime = openTime + slotDuration / 60;
+
+      while (endTime <= closeTime) {
+        const slots = {
+          startTime: { hour: startTime, minute: 0 },
+          endTime: { hour: endTime, minute: 0 },
+        };
+        const slot = {
+          venueId: museum._id,
+          date,
+          slots,
+          maxCapacity,
+          elasticCapacity,
+        };
+        await slotting.createSlot(slot);
+        startTime = endTime;
+        endTime = startTime + slotDuration / 60;
+      }
     }
+
+    console.log("Slots generated successfully");
+    return true;
+  } catch (error) {
+    console.error("Error generating slots:", error);
+    return 0;
+  }
 };
 
-    cron.schedule(
-        '10 0 * * *', // Cron expression for midnight
-        async () => {
-            console.log('Running autoGenerateSlots task at midnight (IST)...');
-            try {
-                if(process.env.NODE_ENV === 'production'){
-                await autoGenerateSlots();
-                console.log('Slots generated successfully');
-                }
-                else{
-                    console.log('Slots not generated in development mode');
-                }
-            } catch (error) {
-                console.error('Error generating slots:', error);
-            }
-        },
-        {
-            scheduled: true,
-            timezone: 'Asia/Kolkata', // Set timezone to IST
-        }
-    );
-    
-   
-
+cron.schedule(
+  "10 0 * * *", // Cron expression for midnight
+  async () => {
+    console.log("Running autoGenerateSlots task at midnight (IST)...");
+    try {
+      if (process.env.NODE_ENV === "production") {
+        await autoGenerateSlots();
+        console.log("Slots generated successfully");
+      } else {
+        console.log("Slots not generated in development mode");
+      }
+    } catch (error) {
+      console.error("Error generating slots:", error);
+    }
+  },
+  {
+    scheduled: true,
+    timezone: "Asia/Kolkata", // Set timezone to IST
+  }
+);

2f7f6899033e80ad542f5127863454c5d3e625c7
sk66641 <sanukumar66641@gmail.com>
2026-01-20 00:42:52 +0530

    Update otpService.js
    gmail SMTP works locally but fails on render with connection timeout on port 465

--- server/services/otpService.js
+++ server/services/otpService.js
@@ -5,8 +5,8 @@ const bcrypt = require('bcrypt');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true, 
+    port: 587,
+    secure: false, 
     auth: {
         user: process.env.HH_EMAIL,
         pass: process.env.HH_PASSWORD
